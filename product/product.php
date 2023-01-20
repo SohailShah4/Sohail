7267481c1020bb920e1e7b5cb3d8994b8ca17c56
SohailShah4 <115266244+SohailShah4@users.noreply.github.com>
2023-01-20 20:35:50 +0500

    Add files via upload

---
+++ product/product.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+    <title>Index</title>
+  </head>
+  <body>
+
+  <div class="container my-3">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="category.php">Category</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="ptype.php">Product Type</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="manufacture.php">Manufacture</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="product.php">Product</a>
+                </li>
+            </ul>
+
+            </div>
+        </div>
+    </nav>
+  </div>
+  </body>
+</html>
